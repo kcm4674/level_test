611641815198e27435009bc984fd3b4ee5e8f9b1
kbs4674 <kbs4674@naver.com>
2019-08-02 15:27:14 +0000

    [LV 2] Devise Gem 설치

--- Gemfile
+++ Gemfile
@@ -60,3 +60,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+## DB 시각화 확인
+gem 'rails_db'
+
+## Devise Gem 설치
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   post 'posts/update/:id' => "posts#update", as: "posts_update"
   delete 'posts/destroy/:id' => "posts#destroy", as: "posts_destroy"
   
+  devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
