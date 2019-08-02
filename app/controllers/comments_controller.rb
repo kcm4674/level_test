3cdd81fd592c2161e31080ba0f04ea71f2a49027
kbs4674 <kbs4674@naver.com>
2019-08-02 16:02:04 +0000

    [LV 4] 비동기 Comment (비동기 작성/비동기 삭제)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,22 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(post_id: params[:post_id], body: params[:co_body])
+    ## [비동기 댓글 설명] 댓글이 작성되면 id값이 부여됩니다.
+    ## 해당 id값은 댓글이 DB에 쓰여진 후, 비동기 상에서 보여줘야할 때 쓰입니다.
+    @comment = Comment.create(post_id: params[:post_id], body: params[:co_body])
     
-    redirect_to posts_show_path(params[:post_id])
+    # redirect_to posts_show_path(params[:post_id])
+    respond_to do |t|
+      t.js
+    end
   end
 
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
     
-    redirect_to posts_show_path(params[:post_id])
+    # redirect_to posts_show_path(params[:post_id])
+    respond_to do |t|
+      t.js
+    end
   end
 end
